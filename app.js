b6384d6cc63516a62bd7b8ce9e48fbf928d47801
HarshaKastala51 <108887619+HarshaKastala51@users.noreply.github.com>
2023-12-10 14:59:06 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello, Node.js!\n');
+  res.end('Hello, Harsha this is a Node.js application!\n');
 });
 
 const PORT = process.env.PORT || 3000;
